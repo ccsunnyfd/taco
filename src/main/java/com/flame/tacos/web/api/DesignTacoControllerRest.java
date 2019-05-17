d4dd3fc46b21f08affee394dc3822575b6810df6
ccsunnyfd <chzhyfd@126.com>
2019-05-17 15:47:52 +0800

    define an post endpoint that creates info(json)

--- src/main/java/com/flame/tacos/web/api/DesignTacoControllerRest.java
+++ src/main/java/com/flame/tacos/web/api/DesignTacoControllerRest.java
@@ -41,4 +41,10 @@ public class DesignTacoControllerRest {
         }
         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
+
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco) {
+        return tacoRepo.save(taco);
+    }
 }
