ec3a36805cef89635d51c595b2cd094ff8552c6b
ccsunnyfd <chzhyfd@126.com>
2019-05-17 15:06:38 +0800

    define an endpoint that returns info(json)

--- src/main/java/com/flame/tacos/dao/TacoRepository.java
+++ src/main/java/com/flame/tacos/dao/TacoRepository.java
@@ -1,9 +1,9 @@
 package com.flame.tacos.dao;
 
 import com.flame.tacos.entity.Taco;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface TacoRepository
-        extends CrudRepository<Taco, Long> {
+        extends PagingAndSortingRepository<Taco, Long> {
 
 }
