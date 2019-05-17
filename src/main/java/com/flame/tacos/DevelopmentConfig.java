031a0ac9bfe323cd7c65d9d114a350c4c929ee0b
ccsunnyfd <chzhyfd@126.com>
2019-05-17 10:38:14 +0800

    exact CommandLineRunner bean and use @Profile

---
+++ src/main/java/com/flame/tacos/DevelopmentConfig.java
@@ -0,0 +1,34 @@
+package com.flame.tacos;
+
+import com.flame.tacos.dao.IngredientRepository;
+import com.flame.tacos.entity.Ingredient;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile({"!prod", "!qa"})
+@Configuration
+public class DevelopmentConfig {
+
+    @Bean
+    public CommandLineRunner dataLoader(IngredientRepository repo) {
+        return new CommandLineRunner() {
+            // because we use jpa, no need to create schema.sql and data.sql,
+            // but we may have to insert initialized data into database manually
+            @Override
+            public void run(String... args) throws Exception {
+                repo.save(new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP));
+                repo.save(new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP));
+                repo.save(new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN));
+                repo.save(new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN));
+                repo.save(new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES));
+                repo.save(new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES));
+                repo.save(new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE));
+                repo.save(new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE));
+                repo.save(new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE));
+                repo.save(new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE));
+            }
+        };
+    }
+}
--- src/main/java/com/flame/tacos/TacoCloudApplication.java
+++ src/main/java/com/flame/tacos/TacoCloudApplication.java
@@ -1,38 +1,12 @@
 package com.flame.tacos;
 
-import com.flame.tacos.dao.IngredientRepository;
-import com.flame.tacos.entity.Ingredient;
-import com.flame.tacos.entity.Ingredient.Type;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication   // <1>
 public class TacoCloudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TacoCloudApplication.class, args); // <2>
-    }
-
-    @Bean
-    public CommandLineRunner dataLoader(IngredientRepository repo) {
-        return new CommandLineRunner() {
-            // because we use jpa, no need to create schema.sql and data.sql,
-            // but we may have to insert initialized data into database manually
-            @Override
-            public void run(String... args) throws Exception {
-                repo.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
-                repo.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
-                repo.save(new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
-                repo.save(new Ingredient("CARN", "Carnitas", Type.PROTEIN));
-                repo.save(new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
-                repo.save(new Ingredient("LETC", "Lettuce", Type.VEGGIES));
-                repo.save(new Ingredient("CHED", "Cheddar", Type.CHEESE));
-                repo.save(new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
-                repo.save(new Ingredient("SLSA", "Salsa", Type.SAUCE));
-                repo.save(new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
-            }
-        };
     }
 }
